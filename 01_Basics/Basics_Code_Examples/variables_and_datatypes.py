5dab4df1d7e61bb66a4ffcb29287a4c052ac9d02
Hamna-Munir <hamnamunir27@gmail.com>
2026-01-02 14:23:05 +0500

    Create variables_and_datatypes.py

---
+++ 01_Basics/Basics_Code_Examples/variables_and_datatypes.py
@@ -0,0 +1,20 @@
+# Variables and Data Types Example
+
+epochs = 50                 # int
+learning_rate = 0.01        # float
+model_name = "LinearModel"  # string
+is_training = True          # boolean
+
+features = [1.2, 2.5, 3.8]  # list
+image_shape = (224, 224, 3) # tuple
+
+hyperparameters = {
+    "epochs": epochs,
+    "learning_rate": learning_rate
+}
+
+unique_labels = {0, 1, 2}
+
+print(type(epochs))
+print(type(features))
+print(type(hyperparameters))
