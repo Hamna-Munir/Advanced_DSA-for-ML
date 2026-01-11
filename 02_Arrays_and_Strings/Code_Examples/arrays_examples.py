80bb6713aeffcb730945666b05152ed875fd9b08
Hamna-Munir <hamnamunir27@gmail.com>
2026-01-11 19:40:41 +0500

    Create arrays_examples.py

---
+++ 02_Arrays_and_Strings/Code_Examples/arrays_examples.py
@@ -0,0 +1,25 @@
+# Arrays Examples
+
+arr = [10, 20, 30, 40, 50]
+
+# Access elements
+print(arr[0])
+print(arr[-1])
+
+# Traverse array
+for value in arr:
+    print(value)
+
+# Update element
+arr[2] = 35
+print(arr)
+
+# Output:
+# 10
+# 50
+# 10
+# 20
+# 30
+# 40
+# 50
+# [10, 20, 35, 40, 50]
