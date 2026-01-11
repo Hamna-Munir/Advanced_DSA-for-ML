677bfe8d0a7f5599a2e58010dab29bc391d6245f
Hamna-Munir <hamnamunir27@gmail.com>
2026-01-11 19:41:29 +0500

    Create strings_examples.py

---
+++ 02_Arrays_and_Strings/Code_Examples/strings_examples.py
@@ -0,0 +1,28 @@
+# Strings Examples
+
+text = "machine learning"
+
+# Access characters
+print(text[0])
+print(text[-1])
+
+# Traverse string
+for char in text[:7]:
+    print(char)
+
+# String operations
+print(text.upper())
+print(text.replace("learning", "vision"))
+
+# Output:
+# m
+# g
+# m
+# a
+# c
+# h
+# i
+# n
+# e
+# MACHINE LEARNING
+# machine vision
