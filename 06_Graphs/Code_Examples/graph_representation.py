5f4ef69e66a887dfc5a73d3e85d917c4a69da8bd
Hamna-Munir <hamnamunir27@gmail.com>
2026-02-01 15:16:36 +0500

    Create graph_representation.py

---
+++ 06_Graphs/Code_Examples/graph_representation.py
@@ -0,0 +1,27 @@
+# Graph Representation using Adjacency List
+
+class Graph:
+    def __init__(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)  # Undirected graph
+
+    def display(self):
+        for node in self.graph:
+            print(f"{node} -> {self.graph[node]}")
+
+
+# Example
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 3)
+g.add_edge(2, 3)
+
+g.display()
