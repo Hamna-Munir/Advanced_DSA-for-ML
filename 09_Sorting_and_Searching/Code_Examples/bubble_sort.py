955b5d2e6330fde943981380483dd03baa243e6b
Hamna-Munir <hamnamunir27@gmail.com>
2026-03-06 21:02:51 +0500

    Create bubble_sort.py

---
+++ 09_Sorting_and_Searching/Code_Examples/bubble_sort.py
@@ -0,0 +1,25 @@
+# Bubble Sort Example
+
+def bubble_sort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+
+    return arr
+
+
+data = [64, 34, 25, 12, 22, 11, 90]
+
+print("Original Array:", data)
+
+sorted_array = bubble_sort(data)
+
+print("Sorted Array:", sorted_array)
+
+
+# Output
+# Original Array: [64, 34, 25, 12, 22, 11, 90]
+# Sorted Array: [11, 12, 22, 25, 34, 64, 90]
